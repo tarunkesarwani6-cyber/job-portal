2fce64d328d956e59031973d4370a4781fba0d13
Tarun <tarunkesarwani6@gmail.com>
2026-08-10 16:55:33 +0530

    Connect frontend to deployed backend

--- frontend/jobportal/src/utils/apiPaths.js
+++ frontend/jobportal/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://127.0.0.1:8000";
+export const BASE_URL = "https://job-portal-backend-m75t.onrender.com";
 export const API_PATHS = {
     AUTH: {
         REGISTER: "/api/auth/register",
